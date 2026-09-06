57d01d2be6b0e1ddb3930e2c40ab6d2a43a3ad52
xie.net <176348291+xie-net@users.noreply.github.com>
2026-09-05 19:58:18 -0500

    Add solutin to data types and conditions exercise 02_addNumbers

--- foundations/data_types_and_conditionals/02_addNumbers/addNumbers.js
+++ foundations/data_types_and_conditionals/02_addNumbers/addNumbers.js
@@ -4,10 +4,11 @@ function addNumbers() {
 
   let result;
 
-  result = "a" + "b"; // <------ EDIT THIS LINE
+  result = a + b; // <------ EDIT THIS LINE
 
   return result;
 }
+
 
 // Do not change this
 module.exports = addNumbers;
