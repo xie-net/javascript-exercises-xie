59a6178fef8053dd72bae8cbf4723f60039954ba
xie.net <176348291+xie-net@users.noreply.github.com>
2026-09-09 19:31:08 -0500

    Add solution to reverse string exercise

--- foundations/loops_and_arrays/02_reverseString/reverseString.js
+++ foundations/loops_and_arrays/02_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    result = '';
+    for (i=str.length;i>=0,i--;) {
+        const char = str.charAt(i);
 
+        result += char;
+    }
+    return result;
 };
 
 // Do not edit below this line
