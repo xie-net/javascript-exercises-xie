2736710f18420519ca10c7186452fcaa3628d74a
xie.net <176348291+xie-net@users.noreply.github.com>
2026-09-09 20:03:12 -0500

    Add solution to sumAll exercise

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if ((!Number.isInteger(num1)) || (!Number.isInteger(num2)) || num1<0 || num2<0) {
+        return "ERROR";
+    }
+    let myNumArray = [];
+    let sum=0;
+    
+    let greaterNumber = Math.max(num1, num2);
 
-};
+    for (let i=(Math.min(num1,num2));i<= greaterNumber;i++) {
+        sum += i;
+    }
+
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
