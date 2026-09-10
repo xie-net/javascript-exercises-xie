bdb8beb9db2249090cb95f8deb47eee4ba3e1970
xie.net <176348291+xie-net@users.noreply.github.com>
2026-09-09 19:15:44 -0500

    Add solution to repeatString exercise

--- foundations/loops_and_arrays/01_repeatString/repeatString.js
+++ foundations/loops_and_arrays/01_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let i=0;
+    let result='';
+    if (num<0) {
+        return "ERROR";
+    }
+    while (i<num) {
+        result += str;
+        i++;
+    }
+    return result;
 };
 
 // Do not edit below this line
