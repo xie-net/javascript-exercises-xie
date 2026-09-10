fababc6f1ce388c6d7b86ddf6d2be61439369323
xie.net <176348291+xie-net@users.noreply.github.com>
2026-09-09 19:47:39 -0500

    Add solutionto removefromArray exercise

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...arrItem) {
+    for (let i=0; i<(myArray.length); i++) {
+        if (arrItem.includes(myArray[i])) {
+            myArray.splice(i,1);
+            i--;
+        }
+    }
+    return myArray;
 };
 
 // Do not edit below this line
